7525a5d31bf83acf83d5b60c4d7c566c7c2c403d
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-01-25 17:05:51 -0300

    Eslint corrigido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
-const zoo = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids1, ids2 ) {
-  const filSpecie = data.species.filter((filSpecies) => filSpecies.id === ids1 || filSpecies.id === ids2);
+function getSpeciesByIds(ids1, ids2) {
+  const filSpecie = data.species
+  .filter((filSpecies) => filSpecies.id === ids1 || filSpecies.id === ids2);
   return filSpecie;
 }
 module.exports = getSpeciesByIds;
