c616a3a4e7819dc6adbf5351e04b01f073b61f6c
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-01-25 17:08:10 -0300

    Eslint corrigido2

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids1, ids2) {
   const filSpecie = data.species
-  .filter((filSpecies) => filSpecies.id === ids1 || filSpecies.id === ids2);
+   .filter((filSpecies) => filSpecies.id === ids1 || filSpecies.id === ids2);
   return filSpecie;
 }
 module.exports = getSpeciesByIds;
