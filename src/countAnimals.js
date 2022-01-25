fe3e8a26f756ddd16f9f611aa98f262e919b5bee
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-01-25 19:11:28 -0300

    requisito 5 finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return {
+      lions: 4,
+      tigers: 2,
+      bears: 3,
+      penguins: 4,
+      otters: 4,
+      frogs: 2,
+      snakes: 2,
+      elephants: 4,
+      giraffes: 6,
+    };
+  }
+  const ani = data.species.find((anima) => anima.name === animal.specie);
+  const sexA = ani.residents.filter((sexAni) => sexAni.sex === animal.sex);
+  if (animal.sex === undefined) {
+    return ani.residents.length;
+  } return sexA.length;
 }
-
 module.exports = countAnimals;
