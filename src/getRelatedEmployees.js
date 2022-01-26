190a82d571a9e4c378afb9209dc21bac6f2a7a43
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-01-26 03:04:20 -0300

    requisito 4 finalizado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managers = [stephanieId, olaId, burlId];
+
 function isManager(id) {
-  // seu código aqui
+  const gerent = managers.includes(id);
+  return gerent;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const filMena = data.employees.filter((mena) => {
+    const trabalhadores = mena.managers.find((menatw) => menatw === managerId);
+    return trabalhadores;
+  });
+  const resp = filMena.map((filMenaa) => `${filMenaa.firstName} ${filMenaa.lastName}`);
+  return resp;
 }
-
+console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 module.exports = { isManager, getRelatedEmployees };
